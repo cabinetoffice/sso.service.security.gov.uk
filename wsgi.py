cb66fffcaecb5b7440094497bd33d26683d2b75b
OllieJC <5426038+OllieJC@users.noreply.github.com>
2023-03-24 01:09:41 +0000

    Add refresh token

--- wsgi.py
+++ wsgi.py
@@ -446,6 +446,7 @@ def auth_token():
 
     token = {
         "access_token": access_token,
+        "refresh_token": access_token,
         "id_token": id_token,
         "token_type": "Bearer",
         "expires_in": 3600,
